d0887114652ad252de3a9ea0281367017a4dee1c
Angus Yu <pighaddt@gmail.com>
2021-08-12 11:29:16 +0800

    add new tabView(figure4) startPoint 1 channel and rms detect

--- MainWindowGUI.py
+++ MainWindowGUI.py
@@ -5,13 +5,28 @@ import serial
 from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog
 from numpy import linspace
 
+import RMSDetect
 from GUI.GUI import serialDemo
 from GUI_EMG_IMU import Ui_MainWindow
 import pyqtgraph as pg
 import numpy as np
+
+import matplotlib
+
+from plotTimeFrequencyDomain import plotTimeFrequencyDomain
+
+matplotlib.use("Qt5Agg")  # 声明使用QT5
+import pandas as pd
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import random
+
+
 
 class SignalCommunicate(PyQt5.QtCore.QObject):
     request_graph_update = PyQt5.QtCore.pyqtSignal()
@@ -21,14 +36,19 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__() #inherit Mainwindow super function
         self.ui = Ui_MainWindow() # your mainwindow from pyuic5
         self.ui.setupUi(self) # your init setupUI function
-        self.p1, self.p2, self.p3, self.p4 = self.set_graph2_ui()  # set the drawing window
+        # self.p1, self.p2, self.p3, self.p4 = self.set_graph2_ui()  # set the drawing window
+        self.p2, self.p3, self.p4 = self.set_graph2_ui()  # set the drawing window
         self.p5, self.p6 = self.set_graph3_ui()  # set the drawing window
         # self.ui.pushButton_StartPlot.clicked.connect(self.threadStart)
         self.ui.pushButton_EMG.clicked.connect(self.connectEMGSP)
         self.ui.pushButton_IMU.clicked.connect(self.connectIMUSP)
-        self.ui.pushButton_StartPlot.clicked.connect(self.threadStart)
-        self.ui.pushButton_ClosePlot.clicked.connect(self.stop_plot)
+        self.ui.pushButton_StartEMGPlot.clicked.connect(self.threadStart)
+        self.ui.pushButton_CloseEMGPlot.clicked.connect(self.stop_plot)
         self.ui.pushButton_searchSP.clicked.connect(self.searchComPort)
+        self.ui.pushButton_LoadEMG.clicked.connect(self.loadEMG)
+        self.ui.pushButton_LoadIMU.clicked.connect(self.loadIMU)
+        self.ui.checkBox_EMGDevice1.clicked.connect(self.EMGDevice1Click)
+        self.ui.checkBox_EMGDevice2.clicked.connect(self.EMGDevice2Click)
         # self.comPortSetting()
         self.EMGIMUDataSetting()
         self.bpsArray = ['4800', '9600', '115200']
@@ -40,6 +60,27 @@ class MainWindow(QtWidgets.QMainWindow):
         self.signalComm = SignalCommunicate()
         # Update graph whenever the 'request_graph_update' signal is emitted
         self.signalComm.request_graph_update.connect(self.update_graph)
+
+        # a figure instance to plot on
+        self.figure1 = plt.figure()
+        self.figure2 = plt.figure()
+        self.figure3 = plt.figure()
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas1 = FigureCanvas(self.figure1)
+        self.canvas2 = FigureCanvas(self.figure2)
+        self.canvas3 = FigureCanvas(self.figure3)
+
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        # self.toolbar = NavigationToolbar(self.canvas, self)
+        # set the layout
+        # layout.addWidget(self.toolbar)
+        self.ui.verticalLayout_3.addWidget(self.canvas1)
+        self.ui.verticalLayout_4.addWidget(self.canvas2)
+        self.ui.verticalLayout_5.addWidget(self.canvas3)
+
+        matplotlib.rcParams.update({'font.size': 8})
 
     def EMGIMUDataSetting(self):
         self.windowWidth_EMG = 1000
@@ -66,6 +107,19 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.comboBox_Device1CP.addItem(self.comPorResult[i])
             self.ui.comboBox_Device2CP.addItem(self.comPorResult[i])
 
+    def reset(self):
+        global curve2, curve3, curve4, curve5, curve6
+        self.ui.comboBox_Device1CP.clear()
+        self.ui.comboBox_Device2CP.clear()
+        self.ui.label_BTConnected_Item.clear()
+        self.comPortEMGName = None
+        self.comPortIMUName = None
+        curve2.clear()
+        curve3.clear()
+        curve4.clear()
+        curve5.clear()
+        curve6.clear()
+
     def connectEMGSP(self):
         self.comPortEMG = self.ui.comboBox_Device1CP.currentText()
         self.baudRateEMG = self.ui.comboBox_Device1BR.currentText()
@@ -89,15 +143,15 @@ class MainWindow(QtWidgets.QMainWindow):
         # pg The drawing window can be added to the graph_layout in the GUI as a widget, and of course can be added to all other Qt containers.
         # self.verticalLayoutWidget.addWidget(win)
         self.ui.verticalLayout.addWidget(win)
-        p1 = win.addPlot(title="EMG Raw Data")  # Add the first drawing window
-        p1.setLabel('left', text='mV', color='#000000')  # y axis setting function
-        p1.showGrid(x=False, y=False)  # Grid setting function
-        p1.setLogMode(x=False, y=False)  # False stands for linear axis, True stands for logarithmic axis
-        p1.setLabel('bottom', text='points', color='#000000', units='s')  # x axis setting function
-        p1.setYRange(min=int(Y_EMG_range[0]), max=int(Y_EMG_range[1]))
-        p1.addLegend() # Select whether to add legend
-
-        curve1 = p1.plot(pen=pg.mkPen(color='k', width=2.0), name="EMG Raw Data")  ##pitch EMG
+        # p1 = win.addPlot(title="EMG Raw Data")  # Add the first drawing window
+        # p1.setLabel('left', text='mV', color='#000000')  # y axis setting function
+        # p1.showGrid(x=False, y=False)  # Grid setting function
+        # p1.setLogMode(x=False, y=False)  # False stands for linear axis, True stands for logarithmic axis
+        # p1.setLabel('bottom', text='points', color='#000000', units='s')  # x axis setting function
+        # p1.setYRange(min=int(Y_EMG_range[0]), max=int(Y_EMG_range[1]))
+        # p1.addLegend() # Select whether to add legend
+        #
+        # curve1 = p1.plot(pen=pg.mkPen(color='k', width=2.0), name="EMG Raw Data")  ##pitch EMG
 
 
         win.nextRow()  # layouts, arranged vertically, without adding this line, the default horizontal arrangement
@@ -133,7 +187,8 @@ class MainWindow(QtWidgets.QMainWindow):
         p4.addLegend()
         curve4 = p4.plot(pen=pg.mkPen(color='k', width=2.0), name="IMU 1 Yaw")  ##pitch EMG
 
-        return p1, p2, p3, p4
+        # return p1, p2, p3, p4
+        return p2, p3, p4
     def set_graph3_ui(self):
         global curve5, curve6
         Y_EMG_range = [-3000, 3000]
@@ -259,6 +314,103 @@ class MainWindow(QtWidgets.QMainWindow):
         print('Thread ={} Function = update_graph()'.format(threading.currentThread().getName()))
         curve5.setData(self.EMGData1)
         curve6.setData(self.EMGData2)
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+                                                  "All Files (*);;csv Files (*.csv)", options=options)
+        if fileName:
+            # print(fileName)
+            return fileName
+
+    def loadEMG(self):
+        global filenameEMG1, rms1, startPointArray
+        filenameEMG1 = self.openFileNameDialog()
+        print("EMG Filename" + filenameEMG1)
+        dataArray = pd.read_csv(filenameEMG1, skiprows=3, usecols=[3])
+        data = dataArray.iloc[:, 0]
+        # beforeMedianFrequency = plotTimeFrequencyDomain(data)
+        # create an axis
+        rms1 = RMSDetect.rms_function(data, 50)
+        startPointArray = RMSDetect.startPoint(rms1, 270)
+        print(startPointArray)
+        self.figure1.clear()
+        ax = self.figure1.add_subplot()
+
+
+        # discards the old graph
+        # ax.hold(False) # deprecated, see above
+
+        # plot data
+        ax.plot(data)
+        self.canvas1.draw()
+
+    def loadIMU(self):
+        filename = self.openFileNameDialog()
+        print("EMG Filename" + filename)
+        dataArray = pd.read_csv(filename, skiprows=3, usecols=[5])
+        data = dataArray.iloc[:, 0]
+        # beforeMedianFrequency = plotTimeFrequencyDomain(data)
+        # create an axis
+        ax = self.figure2.add_subplot(111)
+
+        # discards the old graph
+        # ax.hold(False) # deprecated, see above
+
+        # plot data
+        ax.plot(data)
+        # plt.xticks(range(7), [0, 2000, 4000, 6000, 8000, 10000, 120000])
+        self.canvas2.draw()
+
+    def EMGDevice1Click(self):
+        global filenameEMG1, rms1, startPointArray
+        # filenameEMG1 = 'C:/Users/nobody/Desktop/test_2.csv'
+        if filenameEMG1 is not None:
+            #RMS Detect Function start
+            if self.ui.checkBox_EMGDevice1.isChecked():
+                dataArray = pd.read_csv(filenameEMG1, skiprows=3, usecols=[3])
+                data = dataArray.iloc[:, 0]
+                # beforeMedianFrequency = plotTimeFrequencyDomain(data)
+                # create an axis
+                self.figure1.clear()
+                ax = self.figure1.add_subplot()
+
+                # discards the old graph
+                # ax.hold(False) # deprecated, see above
+                # plot data
+                ax.plot(data)
+                # ax.plot(rms1, 'r')
+                for i in range(len(startPointArray)):
+                    if i%2 == 1:
+                        ax.plot(startPointArray[i], rms1[i], marker="o", color="r")
+                    else:
+                        ax.plot(startPointArray[i], rms1[i], marker="o", color="k")
+
+
+                # ax.plot([5000, 5000], [-300, 300], 'r')
+                # ax.plot([6000, 6000], [-300, 300], 'b')
+                # plt.xticks(range(7), [0, 2000, 4000, 6000, 8000, 10000, 120000])
+                self.canvas1.draw()
+
+            else:
+                dataArray = pd.read_csv(filenameEMG1, skiprows=3, usecols=[3])
+                data = dataArray.iloc[:, 0]
+                # beforeMedianFrequency = plotTimeFrequencyDomain(data)
+                # create an axis
+                self.figure1.clear()
+                ax = self.figure1.add_subplot()
+
+                # discards the old graph
+                # ax.hold(False) # deprecated, see above
+
+                # plot data
+                ax.plot(data)
+                # plt.xticks(range(7), [0, 2000, 4000, 6000, 8000, 10000, 120000])
+                self.canvas1.draw()
+
+    def EMGDevice2Click(self):
+        print("aa")
 
 
 if __name__ == '__main__':
---
+++ RMSDetect.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+def rms_function(data, window):
+    rms = np.array(data)
+
+    halfMask = round(window)
+    for index in range(len(data)):
+        # lower upper setting
+        if index < halfMask:
+            lower = 1
+        else:
+            lower = index - halfMask
+
+        if index > len(data) - halfMask:
+            upper = len(data)
+        else:
+            upper = index + halfMask
+
+        current = data[lower:upper]
+        power2 = current * current
+        mean = power2.mean()
+        sqrt = np.sqrt(mean)
+        rms[index] = sqrt
+
+    return rms
+
+def startPoint(rms, threshold):
+    startPointArray = []
+    isStart = False
+
+    for i in range(len(rms)):
+        if isStart == False and rms[i] > threshold:
+            startIndex = i
+            isStart = True
+        elif isStart == True and rms[i] < threshold:
+            stopIndex = i
+            isStart = False
+            startPointArray.append(startIndex)
+            startPointArray.append(stopIndex)
+
+    return startPointArray
